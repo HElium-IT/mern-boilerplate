c79e7e550045a7b56803f0d88e46bad26262a025
Helium-IT <eliosanics@LAPTOP-I913S407>
2024-01-14 19:43:43 +0100

    WIP styling

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -6,6 +6,7 @@ import registerServiceWorker from "./registerServiceWorker";
 import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
+import "./styles";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
---
+++ client/src/styles/index.ts
@@ -0,0 +1,8 @@
+import './_01-tokens.css';
+import './_02-reset.css';
+import './_03-core.css';
+import './_04-forms.css';
+import './_05-layout.css';
+import './_06-animation.css';
+import './_07-components.css';
+import './_08-utilities.css';
