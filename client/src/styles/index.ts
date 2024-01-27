91784c5d8e4eb8ff10a4361d8c83fae039c014e2
HElium-IT <elio.palomba.dev@gmail.com>
2024-01-27 21:02:50 +0100

    Remove unused CSS styles

--- client/src/styles/index.ts
+++
@@ -1,8 +0,0 @@
-import './_01-tokens.css';
-import './_02-reset.css';
-import './_03-core.css';
-import './_04-forms.css';
-import './_05-layout.css';
-import './_06-animation.css';
-import './_07-components.css';
-import './_08-utilities.css';
